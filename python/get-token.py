84f2f47ab2899678cdc802d67803e8dbb366305e
permoK <perminusk21@gmail.com>
2024-06-16 17:21:02 +0300

    update

--- python/get-token.py
+++ python/get-token.py
@@ -1,8 +1,13 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Credentials
-username = 'peekay'
-password = 'kali'
+username = os.environ.get('USERNAME')
+password = os.environ.get('PASSWORD')
 
 # Obtain the token 
 token_response = requests.post('https://codius.tech/api-token-auth/', data={'username': username, 'password': password })
