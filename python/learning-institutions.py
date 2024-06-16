746319b54df1f6955597b8a71e1f0c8febb277eb
permoK <perminusk21@gmail.com>
2024-06-16 17:15:08 +0300

    update

--- python/learning-institutions.py
+++ python/learning-institutions.py
@@ -8,6 +8,7 @@ token =  os.environ.get('TOKEN')
 
 headers = {'Authorization': f'Token {token}'}
 
+
 response = requests.get('https://codius.tech/api/v1/user/learning-institutions/', headers=headers)
 
 # Process the response
--- python/user-profiles.py
+++ python/user-profiles.py
@@ -8,7 +8,7 @@ token =  os.environ.get('TOKEN')
 
 # Make a request to the protected view with the token
 headers = {'Authorization': f'Token {token}'}
-response = requests.get('http://127.0.0.1:8000/api/v1/user/interests/')
+response = requests.get('http://127.0.0.1:8000/api/v1/user/interests/', headers=headers)
 
 # Process the response
 print(response.json())
